771c3ab02f0822ea111c5cd34fcbf93912535b59
suresh <suresh.pudi94@gmail.com>
2017-11-13 21:20:49 +0530

    commoted

---
+++ @java/src/javainterviewprograms/c1.java
@@ -0,0 +1,15 @@
+package javainterviewprograms;
+
+public class c1 {
+
+	public static void main(String[] args) {
+		String x="techmahendra";
+		String y="";
+		for(int i=x.length()-1;i>0;i--)	{
+			System.out.println(y);
+			
+		}
+		}
+
+		}
+
